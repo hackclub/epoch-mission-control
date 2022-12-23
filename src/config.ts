f9e8dbf66675a779bbbb68117ffd696e38dd7525
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-12-23 12:16:01 +0800

    I can count to 30!

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,9 @@ export default {
   commands: ["/next", "/opendoor"],
   events: ["message", "reaction_added"],
   // actions: ["confirm-team-name-1", "confirm-team-name-2"],
-  actions: [1, 2, 3, 4].map((i) => `confirm-team-name-${i}`),
+  actions: [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+  ].map((i) => `confirm-team-name-${i}`),
   admin: ["UEJL2RADT", "U013B6CPV62"],
 };
